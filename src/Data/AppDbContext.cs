ed121286fa4ece16a70744c30343a3a7c7575cdb
Maciej Gos <maciej@maciejgos.com>
2016-06-27 17:38:57 +0200

    Create AppDbContext

---
+++ src/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyDriving.Data
+{
+    class AppDbContext : DbContext
+    {
+        public DbSet<Model.Vehicle> Vehicles { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Filename=MyDriving.db");
+        }
+    }
+}
