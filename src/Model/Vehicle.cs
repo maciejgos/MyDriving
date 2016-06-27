b74ca89bcf60068ac02be54409991151abe0a129
Maciej Gos <maciej@maciejgos.com>
2016-06-27 17:34:36 +0200

    Vehicle model

---
+++ src/Model/Vehicle.cs
@@ -0,0 +1,15 @@
+﻿namespace MyDriving.Model
+{
+    class Vehicle
+    {
+        public int Id { get; set; }
+
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int ProductionYear { get; set; }
+
+        public int Mileage { get; set; }
+    }
+}
