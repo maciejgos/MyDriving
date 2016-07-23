dd53a7cc1bbab7c3d4ad555feb78712cc2174d6a
Maciej Gos <maciej@maciejgos.com>
2016-07-23 22:43:54 +0200

    Add unit test project

---
+++ MyDriving.UnitTest.Core/CoreTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyDriving.UnitTest.Core
+{
+    [TestClass]
+    public class CoreTests
+    {
+        [TestMethod]
+        public void CanCalculateAverageFuelConsumption()
+        {
+            IEnumerable<Models.Fuelling> collection = new List<Models.Fuelling>
+            {
+                new Models.Fuelling
+                {
+                    Id = 1,
+                    Liters = 40,
+                    Mileage = 180000
+                },
+                new Models.Fuelling
+                {
+                    Id = 2,
+                    Liters = 40,
+                    Mileage = 180420
+                },
+                new Models.Fuelling
+                {
+                    Id = 3,
+                    Liters = 40,
+                    Mileage = 180840
+                }
+
+            };
+            var average = MyDriving.Core.CalculationEngine.Calculate(collection);
+
+            Assert.AreEqual(9.5m, average);
+        }
+
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestMethod]
+        public void ShouldBlockCalculationWhenThereIsNotEnougthRefuel()
+        {
+
+        }
+    }
+}
---
+++ src/Core/CalculationEngine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using MyDriving.Models;
+using System.Linq;
+
+namespace MyDriving.Core
+{
+    public class CalculationEngine
+    {
+        public static decimal Calculate(IEnumerable<Fuelling> collection)
+        {
+            decimal result = 0.0m;
+            var orderedCollection = collection.OrderByDescending(item => item.Id);
+            var arr = orderedCollection.Take(2).ToArray();
+
+            int distance = arr[0].Mileage - arr[1].Mileage;
+            result = (Convert.ToDecimal(arr[0].Liters) / Convert.ToDecimal(distance)) * 100;
+
+            return Math.Round(result,1);
+        }
+    }
+}
