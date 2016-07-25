e4a7742e5e69dd566cd7629dea8ad83d5dad9a67
Maciej Gos <maciej@maciejgos.com>
2016-07-25 23:00:07 +0200

    Create basic CreateVehicleViewModel

---
+++ src/ViewModels/CreateVechicleViewModel.cs
@@ -0,0 +1,15 @@
+﻿using GalaSoft.MvvmLight;
+
+namespace MyDriving.ViewModels
+{
+    public class CreateVechicleViewModel : ViewModelBase
+    {
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int YearOfManufacture { get; set; }
+
+        public int Mileage { get; set; }
+    }
+}
