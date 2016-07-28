a71bedd3a3fc3a2469722baec25780ad5b903560
Maciej Gos <maciej@maciejgos.com>
2016-07-28 21:56:18 +0200

    NavigationServiceExtension

---
+++ src/Extensions/NavigationServiceExtension.cs
@@ -0,0 +1,14 @@
+﻿using GalaSoft.MvvmLight.Views;
+using MyDriving.Views;
+
+namespace MyDriving.Extensions
+{
+    public static class NavigationServiceExtension
+    {
+        public static void ConfigureRoutes(this NavigationService service)
+        {
+            service.Configure(Routes.MainPage, typeof(MainPage));
+            service.Configure(Routes.CreateVehiclePage, typeof(CreateVehiclePage));
+        }
+    }
+}
