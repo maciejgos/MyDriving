8093b8a062ecfd861307a0f6363341b397f20545
Maciej Gos <maciej@maciejgos.com>
2016-07-30 16:09:19 +0200

    Code cleanup at CreateVehiclePage

--- src/Views/CreateVehiclePage.xaml.cs
+++ src/Views/CreateVehiclePage.xaml.cs
@@ -1,18 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Core;
+﻿using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -38,32 +26,5 @@ namespace MyDriving.Views
                 e.Handled = true;
             }
         }
-
-        //private async void SaveButton_Click(object sender, RoutedEventArgs e)
-        //{
-        //    //TODO: Validate input
-        //    var entity = new Models.Vehicle
-        //    {
-        //        Make = TextBoxMake.Text,
-        //        Model = TextBoxModel.Text,
-        //        ProductionYear = int.Parse(TextBoxAge.Text),
-        //        Mileage = int.Parse(TextBoxMileage.Text)
-        //    };
-
-        //    Data.AppDbContext.Instance.Vehicles.Add(entity);
-        //    await Data.AppDbContext.Instance.SaveChangesAsync();
-
-        //    Frame.GoBack();
-        //}
-
-        //private void CancelButton_Click(object sender, RoutedEventArgs e)
-        //{
-        //    Frame.GoBack();
-        //}
-
-        //private void AddPhotoButton_Click(object sender, RoutedEventArgs e)
-        //{
-        //    throw new NotImplementedException();
-        //}
     }
 }
