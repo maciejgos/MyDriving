42441fec70934c60a4393902c1e9dc6d949a49ff
Maciej Gos <maciej@maciejgos.com>
2016-08-06 23:07:50 +0200

    Add basic entities
    Add Vehicle and Refuel class

---
+++ src/MyDriving.Core/Models/Refuel.cs
@@ -0,0 +1,9 @@
+﻿namespace MyDriving.Core.Models
+{
+    public class Refuel
+    {
+        public int Id { get; set; }
+        public int Liters { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ src/MyDriving.Core/Models/Vehicle.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace MyDriving.Core.Models
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int YearOfManufacture { get; set; }
+        public int Mileage { get; set; }
+
+        public IEnumerable<Refuel> Refuels { get; set; }
+    }
+}
