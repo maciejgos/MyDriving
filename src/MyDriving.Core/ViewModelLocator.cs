34107fcafc7619862ff6d8dfb553f6a2d32c1be8
Maciej Gos <maciej@maciejgos.com>
2016-08-07 09:43:53 +0200

    Create ViewModelLocator

---
+++ src/MyDriving.Core/ViewModelLocator.cs
@@ -0,0 +1,24 @@
+﻿using GalaSoft.MvvmLight.Ioc;
+using Microsoft.Practices.ServiceLocation;
+using MyDriving.Core.ViewModels;
+
+namespace MyDriving.Core
+{
+    public class ViewModelLocator
+    {
+        public AppShellViewModel AppShellViewModel
+        {
+            get
+            {
+                return ServiceLocator.Current.GetInstance<AppShellViewModel>();
+            }
+        }
+
+        static ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+
+            SimpleIoc.Default.Register<AppShellViewModel>();
+        }
+    }
+}
